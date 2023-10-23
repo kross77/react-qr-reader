575e7b8044913985214942eefad53d2319fb9c75
Rahat Zhanshugurov <zhanshugurov.dev@gmail.com>
2023-10-23 23:18:27 +0600

    some changes

--- dist/cjs/QrReader/helpers/cleanup.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const clearPreview: (preview: HTMLVideoElement | any) => void;
-export declare const clearStreams: (streams: MediaStream[]) => void;
-export declare const clearFrames: (cancelIds: number[]) => void;
--- dist/cjs/types/QrReader/helpers/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const getDeviceId: (facingMode: VideoFacingModeEnum) => Promise<string>;
-export declare const decodeQR: ({ data, width, height }: ImageData) => any;
